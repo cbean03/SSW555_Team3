84b9dc9080c90d123ecb72b9ddecf00ca1db01e3
Eric Fitzpatrick <efitzpa1@stevens.edu>
2016-09-19 21:03:02 -0400

    Committing changes for user story 5 implementation. Changed GEDCOM file reading to function 'readGEDCOM(filename)' for easier unit testing. Created function 'isAfterDate(date1, date2)' to check if date1 is after date2. Created directory 'gedcom_test_files' to store user story unit tests. Edited addMarriage Family function for us05.

--- project_team3_oo.py
+++ project_team3_oo.py
@@ -2,11 +2,12 @@
 #This program shall read date from a GEDCOM file and store information for
 #individuals and families in lists
 import datetime, re
-file = "GEDCOMFile.ged"
+
 tags0_norm = ['HEAD', 'TRLR', 'NOTE']
 tags0_id = ['INDI', 'FAM']
 tags1 = ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHIL', 'DIV']
 tags2 = ['DATE']
+today = datetime.datetime.now().date()
 
 #Natural sorting function just to meet sorted requirements of project 3 submission
 def natural_sort(l): 
@@ -37,9 +38,15 @@ class Family:
 	def addChild(self, child):
 		self.children.append(child)
 		Family.members += 1
-		
+
 	def addMarried(self, date):
-		self.married = datetime.datetime.strptime(" ".join(date), '%d %b %Y').date()
+		marriage_date = datetime.datetime.strptime(" ".join(date), '%d %b %Y').date()
+		#BEGIN: US05 - Marriage before death
+		if(isAfterDate(marriage_date, self.husband.death) or isAfterDate(marriage_date, self.wife.death)):
+			print "ERROR: User story 5 - Marriage date " + str(marriage_date) + " for family " + self.id + " after death of spouse"
+		#END: US05
+		else:
+			self.married = marriage_date
 		
 	def addDivorced(self, date):
 		self.divorced = datetime.datetime.strptime(" ".join(date), '%d %b %Y').date()
@@ -54,11 +61,14 @@ class Family:
 		print "Husband: " + self.husband.name
 		print "Wife: " + self.wife.name
 		print "Children:",
-		for child in self.children:
-			if child != self.children[-1]:
-				print child.name + ",",
-			else:
-				print child.name
+		if self.children == []:
+			print "None"
+		else:
+			for child in self.children:
+				if child != self.children[-1]:
+					print child.name + ",",
+				else:
+					print child.name
 		
 #Class for holding individual information
 class Individual:
@@ -96,90 +106,106 @@ class Individual:
 		print "Death: " + str(self.death)
 		print "Child of family: " + self.famc
 		print "Spouse of family: " + self.fams
-		
-today = datetime.datetime.now().date()
-curr_indi = ''
-curr_fam = ''
-bday_flag = 0
-deat_flag = 0
-marr_flag = 0
-div_flag = 0
-individuals = {}
-families = {}
-		
-with open(file) as f:
-	for line in f:
-		parts = line.split()
-		
-		#For first level, check for individual/family tag and create new instance
-		if parts[0] == '0':
-			if parts[1] in tags0_norm:
-				pass
-			elif parts[2] in tags0_id:
-				if parts[2] == 'INDI':
-					curr_indi = parts[1].replace("@","")
-					individuals[curr_indi] = Individual(curr_indi)
-					curr_fam = ''
+	
+def isAfterDate(date1, date2):
+	"""Function:     isAfterDate
+	   Purpose:      Returns whether date1 is after date2. If date1 or date2 is of type None then return False.
+	   Parameters:   date1 - datetime.date object | date2 - datetime.date object
+	   Return value: Boolean
+	"""
+	if(date1 != None and date2 != None):
+		return date1 > date2
+	else:
+		return False
+
+def readGEDCOM(filename):
+	curr_indi = ''
+	curr_fam = ''
+	bday_flag = 0
+	deat_flag = 0
+	marr_flag = 0
+	div_flag = 0
+	individuals = {}
+	families = {}
+
+	print "BEGIN: " + filename
+	with open(filename) as f:
+		for line in f:
+			parts = line.split()
+			
+			#For first level, check for individual/family tag and create new instance
+			if parts[0] == '0':
+				if parts[1] in tags0_norm:
+					pass
+				elif parts[2] in tags0_id:
+					if parts[2] == 'INDI':
+						curr_indi = parts[1].replace("@","")
+						individuals[curr_indi] = Individual(curr_indi)
+						curr_fam = ''
+					else:
+						curr_fam = parts[1].replace("@","")
+						families[curr_fam] = Family(curr_fam)
+						curr_indi = ''
 				else:
-					curr_fam = parts[1].replace("@","")
-					families[curr_fam] = Family(curr_fam)
-					curr_indi = ''
-			else:
-				print("Invalid tag")
-				
-		#Add appropriate information to current individual or current family
-		elif parts[0] == '1':
-			if parts[1] in tags1 and curr_indi != '':
-				if parts[1] == 'NAME':
-					individuals[curr_indi].addName(" ".join(parts[2:]).replace('/',''))
-				elif parts[1] == 'SEX':
-					individuals[curr_indi].addSex(parts[2])
-				elif parts[1] == 'BIRT':
-					bday_flag = 1
-				elif parts[1] == 'DEAT':
-					deat_flag = 1
-				elif parts[1] == 'FAMC':
-					individuals[curr_indi].addFamc(parts[2].replace("@",""))
-				elif parts[1] == 'FAMS':
-					individuals[curr_indi].addFams(parts[2].replace("@",""))
-			elif parts[1] in tags1 and curr_fam != '':
-				if parts[1] == 'HUSB':
-					families[curr_fam].addHusband(individuals[parts[2].replace("@","")])
-				elif parts[1] == 'WIFE':
-					families[curr_fam].addWife(individuals[parts[2].replace("@","")])
-				elif parts[1] == 'CHIL':
-					families[curr_fam].addChild(individuals[parts[2].replace("@","")])
-				elif parts[1] == 'MARR':
-					marr_flag = 1
-				elif parts[1] == 'DIV':
-					div_flag = 1
-				
-		#Add appropriate date to current individual or current family
-		elif parts[0] == '2':
-			if parts[1] in tags2 and curr_indi != '':
-				if bday_flag == 1:
-					individuals[curr_indi].addBirthday(parts[2:])
-					bday_flag = 0
-				elif deat_flag == 1:
-					individuals[curr_indi].addDeath(parts[2:])
-					deat_flag = 0
-			elif parts[1] in tags2 and curr_fam != '':
-				if marr_flag == 1:
-					families[curr_fam].addMarried(parts[2:])
-					marr_flag = 0
-				elif div_flag == 1:
-					families[curr_fam].addDivorced(parts[2:])
-					div_flag = 0
+					print("Invalid tag")
 					
-#Print out all loaded information for troubleshooting
-sorted_keys = natural_sort(individuals)
-for i in sorted_keys:
-	individuals[i].printIndividual()
+			#Add appropriate information to current individual or current family
+			elif parts[0] == '1':
+				if parts[1] in tags1 and curr_indi != '':
+					if parts[1] == 'NAME':
+						individuals[curr_indi].addName(" ".join(parts[2:]).replace('/',''))
+					elif parts[1] == 'SEX':
+						individuals[curr_indi].addSex(parts[2])
+					elif parts[1] == 'BIRT':
+						bday_flag = 1
+					elif parts[1] == 'DEAT':
+						deat_flag = 1
+					elif parts[1] == 'FAMC':
+						individuals[curr_indi].addFamc(parts[2].replace("@",""))
+					elif parts[1] == 'FAMS':
+						individuals[curr_indi].addFams(parts[2].replace("@",""))
+				elif parts[1] in tags1 and curr_fam != '':
+					if parts[1] == 'HUSB':
+						families[curr_fam].addHusband(individuals[parts[2].replace("@","")])
+					elif parts[1] == 'WIFE':
+						families[curr_fam].addWife(individuals[parts[2].replace("@","")])
+					elif parts[1] == 'CHIL':
+						families[curr_fam].addChild(individuals[parts[2].replace("@","")])
+					elif parts[1] == 'MARR':
+						marr_flag = 1
+					elif parts[1] == 'DIV':
+						div_flag = 1
+					
+			#Add appropriate date to current individual or current family
+			elif parts[0] == '2':
+				if parts[1] in tags2 and curr_indi != '':
+					if bday_flag == 1:
+						individuals[curr_indi].addBirthday(parts[2:])
+						bday_flag = 0
+					elif deat_flag == 1:
+						individuals[curr_indi].addDeath(parts[2:])
+						deat_flag = 0
+				elif parts[1] in tags2 and curr_fam != '':
+					if marr_flag == 1:
+						families[curr_fam].addMarried(parts[2:])
+						marr_flag = 0
+					elif div_flag == 1:
+						families[curr_fam].addDivorced(parts[2:])
+						div_flag = 0
+
+	#Print out all loaded information for troubleshooting
+	sorted_keys = natural_sort(individuals)
+	for i in sorted_keys:
+		individuals[i].printIndividual()
+		print
 	print
 
-print
-	
-sorted_keys = natural_sort(families)
-for i in sorted_keys:
-	families[i].printFamily()
+	sorted_keys = natural_sort(families)
+	for i in sorted_keys:
+		families[i].printFamily()
+		print
+	print "END: " + filename
 	print
+
+readGEDCOM('GEDCOMFile.ged')
+readGEDCOM('gedcom_test_files\us05.ged')
